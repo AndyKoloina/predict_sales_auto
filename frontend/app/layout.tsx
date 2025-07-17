31e10b32d1361ef0805c524c1554ed2691fa5868
Andy Ranaivo <andyranaivo861@gmail.com>
2025-07-17 16:07:32 +0100

    fix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,16 +1,28 @@
 'use client'; // On ajoute cette ligne pour pouvoir utiliser le state de React
 
-import { useState } from 'react';
+import { useState, useEffect  } from 'react';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import DocumentationSlideout from '../components/DocumentationSlideout';
 import CircuitBackground from '../components/CircuitBackground';
+import LoadingScreen from '@/components/LoadingScreen';
 const inter = Inter({ subsets: ['latin'] });
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [isDocsOpen, setIsDocsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    // Ce minuteur simule un chargement de 2 secondes.
+    // Dans une vraie application, vous pourriez attendre le chargement de données réelles.
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // 2000 millisecondes = 2 secondes
+
+    // Nettoyer le minuteur si le composant est démonté
+    return () => clearTimeout(timer);
+  }, []); // Le tableau vide assure que l'effet ne s'exécute qu'une fois au montage
 
   // Fonctions pour contrôler le panneau
   const openDocs = () => setIsDocsOpen(true);
@@ -19,13 +31,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr">
       <body className={`${inter.className} bg-slate-950 text-slate-200`} suppressHydrationWarning={true}>
-        {/* On passe la fonction d'ouverture au Navbar */}
-        <CircuitBackground />
-        <Navbar onDocsClick={openDocs} />
-        <main className="container mx-auto px-6 py-12">{children}</main>
-
-        {/* On rend le panneau de documentation et on lui passe son état et sa fonction de fermeture */}
-        <DocumentationSlideout isOpen={isDocsOpen} onClose={closeDocs} />
+        {/* ✅ AJOUT : Affichage conditionnel */}
+        {isLoading ? (
+          <LoadingScreen />
+        ) : (
+          <>
+            <CircuitBackground />
+            <Navbar onDocsClick={openDocs} />
+            <main className="container mx-auto px-6 py-12">{children}</main>
+            <DocumentationSlideout isOpen={isDocsOpen} onClose={closeDocs} />
+          </>
+        )}
       </body>
     </html>
   );
